41072e76f4894eb9d0270228ef6a9af2b7158ca7
Nadir N. Mammadaliyev <MammadaliyevNN@kapitalho.az>
2022-11-13 02:23:15 +0400

    adding DbContext

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace MagicVilla_VillaAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Villa> Villas { get; set; }
+}
