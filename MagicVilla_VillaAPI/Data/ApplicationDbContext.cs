e1fe175872efd6298ec852dfef7610b9377795f5
nadir <nadirmemmedeliyev6@gmail.com>
2022-11-13 14:56:51 +0400

    connecting database

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -6,5 +6,9 @@ namespace MagicVilla_VillaAPI.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
     public DbSet<Villa> Villas { get; set; }
 }
